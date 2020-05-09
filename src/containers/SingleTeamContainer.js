bec2ba53a3b78c2fb5f8a7d35e3b892be1f4b97f
mdiaz26 <mike.diaz006@gmail.com>
2020-05-09 13:26:31 -0400

    lots of loops but it works

--- src/containers/SingleTeamContainer.js
+++ src/containers/SingleTeamContainer.js
@@ -11,9 +11,8 @@ class SingleTeamContainer extends React.Component {
         let rosterConfig = this.props.currentDraft.roster_config
         let i = 2
         let newArray = []
-        // while (i < keys.length - 2) {
         while (i < keys.length - 2) {
-            if (!rosterConfig[keys[i]] || rosterConfig[keys[i]] === 0) {
+            if (!rosterConfig[keys[i]] || rosterConfig[keys[i]] === 0 || keys[i] === 'bench') {
                 i++
             } else if (rosterConfig[keys[i]] === 1) {
                 newArray.push(this.keyToObject(keys[i], '', '', false))
@@ -34,138 +33,136 @@ class SingleTeamContainer extends React.Component {
 
     populateRosterObject = franchisePlayers => {
         let rosterObj = this.objectKeysToArray()
-        let rosterCopy = [...rosterObj]
+        // let rosterCopy = [...rosterObj]
         let playersIndex = 0
-        let rosterIndex = 0
+        let rosterIndex
         // let rosterCheckpoint = 0
         
         while (playersIndex < franchisePlayers.length) {
-            // if we're going to look at a slot that's already been filled, we'll
-            // go back to the beginning for safety
+            rosterIndex = 0
+            let stopper = playersIndex + 1
+            // we'll try a double loop just to get it working.
+            // loop through each player and for each player loop through the array until
+            //there's an open spot that matches their position
             // if (rosterObj[rosterIndex].filled) {rosterIndex = rosterCheckpoint}
             if (rosterObj[rosterIndex].filled) {rosterIndex++}
-            
-            if (
-                !rosterObj[rosterIndex].filled
-                &&
-                franchisePlayers[playersIndex].position === 'QB' 
-                && 
-                (rosterObj[rosterIndex] === 'QB' || rosterObj[rosterIndex] === 'S/F')
-            ) {
-                rosterObj[rosterIndex] = this.keyToObject(
-                    rosterObj[rosterIndex].position,
-                    franchisePlayers[playersIndex].player.name,
-                    franchisePlayers[playersIndex].salary,
-                    true
-                )
+
+            if (franchisePlayers[playersIndex].player.position === 'QB') {
+                console.log(rosterObj, rosterIndex, rosterObj[rosterIndex])
+                while (playersIndex < stopper) {
+                    if (rosterIndex > rosterObj.length - 1) {
+                        this.addToBench(rosterObj, franchisePlayers[playersIndex])
+                        playersIndex++
+                    } else if (!rosterObj[rosterIndex].filled &&
+                        (rosterObj[rosterIndex].rosterPosition === 'QB' || rosterObj[rosterIndex].rosterPosition === 'S/F')) {
+                            rosterObj[rosterIndex] = this.keyToObject(
+                                rosterObj[rosterIndex].rosterPosition,
+                                franchisePlayers[playersIndex].player.name,
+                                franchisePlayers[playersIndex].salary,
+                                true
+                            )
+                            playersIndex++
+                    }
+                    rosterIndex++
+                }
+            } else if (franchisePlayers[playersIndex].player.position === 'RB') {
+                console.log(rosterObj, rosterIndex, rosterObj[rosterIndex])
+                while (playersIndex < stopper) {
+                    if (rosterIndex > rosterObj.length - 1) {
+                        this.addToBench(rosterObj, franchisePlayers[playersIndex])
+                        playersIndex++
+                    } else if (!rosterObj[rosterIndex].filled &&
+                        (rosterObj[rosterIndex].rosterPosition === 'RB' || rosterObj[rosterIndex].rosterPosition === 'RB/WR')){
+                            rosterObj[rosterIndex] = this.keyToObject(
+                                rosterObj[rosterIndex].rosterPosition,
+                                franchisePlayers[playersIndex].player.name,
+                                franchisePlayers[playersIndex].salary,
+                                true
+                            )
+                            playersIndex++
+                        }
+                    rosterIndex++
+                }
+            } else if (franchisePlayers[playersIndex].player.position === 'WR') {  
+                console.log(rosterObj, rosterIndex, rosterObj[rosterIndex])
+                while (playersIndex < stopper) {
+                    if (rosterIndex > rosterObj.length - 1) {
+                        this.addToBench(rosterObj, franchisePlayers[playersIndex])
+                        playersIndex++
+                    } else if (!rosterObj[rosterIndex].filled &&
+                        (rosterObj[rosterIndex].rosterPosition === 'WR' || rosterObj[rosterIndex].rosterPosition === 'RB/WR' || rosterObj[rosterIndex].rosterPosition === 'WR/TE')){
+                        rosterObj[rosterIndex] = this.keyToObject(
+                            rosterObj[rosterIndex].rosterPosition,
+                            franchisePlayers[playersIndex].player.name,
+                            franchisePlayers[playersIndex].salary,
+                            true
+                        )
+                        playersIndex++
+                    }
+                    rosterIndex++
+                }
+            } else if (franchisePlayers[playersIndex].player.position === 'TE') {
+                console.log(rosterObj, rosterIndex, rosterObj[rosterIndex])
+                while (playersIndex < stopper) {
+                    if (rosterIndex > rosterObj.length - 1) {
+                        this.addToBench(rosterObj, franchisePlayers[playersIndex])
+                        playersIndex++
+                    } else if (!rosterObj[rosterIndex].filled &&
+                        (rosterObj[rosterIndex].rosterPosition === 'TE' || rosterObj[rosterIndex].rosterPosition === 'WR/TE')){
+                        rosterObj[rosterIndex] = this.keyToObject(
+                            rosterObj[rosterIndex].rosterPosition,
+                            franchisePlayers[playersIndex].player.name,
+                            franchisePlayers[playersIndex].salary,
+                            true
+                        )
+                    playersIndex++
+                    }
                 rosterIndex++
-                // rosterCheckpoint++
-            } else if (
-                !rosterObj[rosterIndex].filled
-                &&
-                franchisePlayers[playersIndex].position === 'RB' 
-                && 
-                (rosterObj[rosterIndex] === 'RB' || rosterObj[rosterIndex] === 'RB/WR')
-            ) {
-                rosterObj[rosterIndex] = this.keyToObject(
-                    rosterObj[rosterIndex].position,
-                    franchisePlayers[playersIndex].player.name,
-                    franchisePlayers[playersIndex].salary,
-                    true
-                )
+                }
+            } else if (franchisePlayers[playersIndex].player.position === 'DST') {
+                console.log(rosterObj, rosterIndex, rosterObj[rosterIndex])
+                while (playersIndex < stopper) {
+                    if (rosterIndex > rosterObj.length - 1) {
+                        this.addToBench(rosterObj, franchisePlayers[playersIndex])
+                        playersIndex++
+                    } else if (!rosterObj[rosterIndex].filled && rosterObj[rosterIndex].rosterPosition === 'DEF') {
+                        rosterObj[rosterIndex] = this.keyToObject(
+                            rosterObj[rosterIndex].rosterPosition,
+                            franchisePlayers[playersIndex].player.name,
+                            franchisePlayers[playersIndex].salary,
+                            true
+                        )
+                    playersIndex++
+                    }
                 rosterIndex++
-                // rosterCheckpoint++
-            } else if (
-                !rosterObj[rosterIndex].filled
-                &&
-                franchisePlayers[playersIndex].position === 'WR' 
-                && 
-                (rosterObj[rosterIndex] === 'WR' || rosterObj[rosterIndex] === 'RB/WR' || rosterObj[rosterCopy] === 'WR/TE')
-            ) {
-                rosterObj[rosterIndex] = this.keyToObject(
-                    rosterObj[rosterIndex].position,
-                    franchisePlayers[playersIndex].player.name,
-                    franchisePlayers[playersIndex].salary,
-                    true
-                )
+                }
+            } else if (franchisePlayers[playersIndex].player.position === 'K') {
+                console.log(rosterObj, rosterIndex, rosterObj[rosterIndex])
+                while (playersIndex < stopper) {
+                    if (rosterIndex > rosterObj.length - 1) {
+                        this.addToBench(rosterObj, franchisePlayers[playersIndex])
+                        playersIndex++
+                    } else if (!rosterObj[rosterIndex].filled && rosterObj[rosterIndex].rosterPosition === 'K') {
+                        rosterObj[rosterIndex] = this.keyToObject(
+                            rosterObj[rosterIndex].rosterPosition,
+                            franchisePlayers[playersIndex].player.name,
+                            franchisePlayers[playersIndex].salary,
+                            true
+                        )
+                    playersIndex++
+                    }
                 rosterIndex++
-                // rosterCheckpoint++
-            } else if (
-                !rosterObj[rosterIndex].filled
-                &&
-                franchisePlayers[playersIndex].position === 'TE' 
-                && 
-                (rosterObj[rosterIndex] === 'TE' || rosterObj[rosterCopy] === 'WR/TE')
-            ) {
-                rosterObj[rosterIndex] = this.keyToObject(
-                    rosterObj[rosterIndex].position,
-                    franchisePlayers[playersIndex].player.name,
-                    franchisePlayers[playersIndex].salary,
-                    true
-                )
-                rosterIndex++
-                // rosterCheckpoint++
-            } else if (
-                !rosterObj[rosterIndex].filled
-                &&
-                franchisePlayers[playersIndex].position === 'DST' 
-                && 
-                rosterObj[rosterIndex] === 'DST'
-            ) {
-                rosterObj[rosterIndex] = this.keyToObject(
-                    rosterObj[rosterIndex].position,
-                    franchisePlayers[playersIndex].player.name,
-                    franchisePlayers[playersIndex].salary,
-                    true
-                )
-                rosterIndex++
-                // rosterCheckpoint++
-            } else if (
-                !rosterObj[rosterIndex].filled
-                &&
-                franchisePlayers[playersIndex].position === 'K' 
-                && 
-                rosterObj[rosterIndex] === 'K'
-            ) {
-                rosterObj[rosterIndex] = this.keyToObject(
-                    rosterObj[rosterIndex].position,
-                    franchisePlayers[playersIndex].player.name,
-                    franchisePlayers[playersIndex].salary,
-                    true
-                )
-                rosterIndex++
-                // rosterCheckpoint++
-            } else {
-                rosterObj[rosterIndex] = this.keyToObject(
-                    rosterObj[rosterIndex].position,
-                    franchisePlayers[playersIndex].player.name,
-                    franchisePlayers[playersIndex].salary,
-                    true
-                )
-                rosterIndex++
-                // rosterCheckpoint++
-            }
-        playersIndex++
-        rosterIndex = 0
+                }
+            } 
         }
+        console.log(rosterObj)
     }
 
-    franchisePlayers = () => {
-        return this.props.franchiseFocus.franchise_players
-        // NEXT STEPS:
-        // create an array of objects. It should look like this:
-        // [{rosterPosition: 'QB', playerName: "", playerSal: ""}, {rosterPosition: 'RB', playerName: "David Johnson", playerSal: "65"}]
-        // Iterate through the array. 
-        // On each key, iterate through the array of players and see if any match the position (map within map)
-        // if player.position === keys[index], return {keys[index]: player}
-
-
+    addToBench = (rosterArray, player) => {
+        let benchPlayerObj = this.keyToObject('BENCH', player.player.name, player.salary, true)
+        rosterArray.push(benchPlayerObj)
     }
 
-    // rosterArray = () => {
-    //     const keys = Object.keys(this.props.currentDraft.roster_config)
-    //     const fPlayers = this.props.franchiseFocus.franchise_players
-    //     fPlayers.map(player => )
 
     render() {
         return (
@@ -173,7 +170,7 @@ class SingleTeamContainer extends React.Component {
                 {this.props.franchiseFocus !== "" ? 
                     <React.Fragment>
                         <h2>{this.props.franchiseFocus.name}</h2>
-                        <button onClick={this.objectKeysToArray}>test roster array</button>
+                        <button onClick={() => this.populateRosterObject(this.props.franchiseFocus.franchise_players)}>test roster array</button>
                         <ol>
                             {this.props.franchiseFocus.franchise_players.map((fPlayer, idx) => (
                                 <li key={idx}>
